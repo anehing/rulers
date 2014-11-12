a37f8a331120a364fccfd90654e98e51865dd9ee
anehing <aneing@gmai.com>
2014-11-12 10:36:36 +0800

    merge

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -3,6 +3,6 @@ module Rulers
     string.gsub(/::/,'/').
       gsub(/([A-Z]+)([A-Z][AA-Z])/,'\1_\2').
       gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("_","_".).downcase
+      tr("_","_").downcase
   end
 end
